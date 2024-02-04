fe5b46969dcd7ad6bea5537dcb71712895e138e6
zirbest <zirbst@gmail.com>
2024-02-04 23:39:28 +0100

    feat: add `duration comparator`

---
+++ src/lib/utils.ts
@@ -0,0 +1,51 @@
+export type req = {
+  duration: string
+}
+
+export async function getData(req: req) {
+  'use server'
+  const amanaRes = await amana(req)
+  const lalgeriennevieRes = await lalgeriennevie(req)
+
+  return {
+    amana: amanaRes?.value,
+    lalgeriennevie: lalgeriennevieRes?.value,
+  }
+}
+
+export async function amana(req: req) {
+  const options = {
+    method: 'GET',
+  }
+
+  try {
+  const rez = await fetch(`https://digit.amana.dz/amana_tarification.simulationvoyage/simulation-voyage/coupon-code/null/type-contrat-id/510/produitId/1/destination-id/82/duree-jours/${req.duration}/convention-id/1/age/20/nbrs-personne-individuel/1/nbrs-personne-tranche1/0/nbrs-personne-tranche2/0/nbrs-personne-tranche3/0/nbrs-personne-tranche4/0?ticket=false`, options)
+
+    const json = await rez.json()
+
+    return  {
+      from: 'amana',
+      value: json.list_formule?.[0]?.montantPrime
+    }
+  } catch(e) {
+    console.error('amana error | ', e)
+  }
+}
+
+export async function lalgeriennevie(req: req) {
+  const options = {
+    method: 'GET',
+  }
+
+  try {
+    const rez = await fetch(`https://lalgeriennevie.dz/wp-content/uploads/forms/vdevis.php?tsous=2&nom=nom&pnom=prenom&rss=&mail=email%40example.com&tel=0777777777&adr=adresse&ddn=1999-10-10&dst=A&pport=passeport&ddef=2024-01-31&dure=${req.duration}&getTotal=1`, options)
+
+    const json = await rez.json()
+    return  {
+      from: 'lalgeriennevie',
+      value: json.total
+    }
+  } catch(e) {
+    console.error('lalgeriennevie error | ', e)
+  }
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,30 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
+import { createStore } from "solid-js/store";
+import { getData, req } from "~/lib/utils";
 
 export default function Home() {
+  const [data, setData] = createStore()
+
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+    <main class="mx-auto text-gray-700 p-4">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault()
+          // @ts-ignore
+          const data = new FormData(e.target)
+          const formObject = Object.fromEntries(data.entries()) as req
+          console.log(formObject)
+
+          const comparators = await getData(formObject)   
+          setData(comparators)
+        }}
+        method="post"
+      >
+        <input class="border" type="text" name="duration" />
+        <button type="submit" > submit </button>
+      </form>
+      <pre>
+        { JSON.stringify(data, null, 2) }
+      </pre>
     </main>
   );
 }
